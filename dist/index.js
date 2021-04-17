17d0e98d9e694aa94ae2f01fcfd1c139ae9acd98
Jacob Haitsma <jacobhaitsma@gmail.com>
2021-04-16 20:08:14 -0400

    :package: Build out data gathering, issue filing

--- dist/index.js
+++ dist/index.js
@@ -6046,16 +6046,48 @@ const core = __nccwpck_require__(186);
 const github = __nccwpck_require__(438);
 
 async function run() {
+  // Constants
+  const REPO_OWNER = 'JacobHaitsma';
+  const REPO_KEY_SUFFIX = '-auctionsoftware';
+  const CLIENTS_REPO = 'test-clients-repo';
+  const CLIENTS_REPO_CUSTOM_ENV_PATH = 'custom_env';
+
+  // Inputs
   const MACHINE_WORKER_TOKEN = core.getInput('MACHINE_WORKER_TOKEN');
+  const issueTitle = core.getInput('issue_title');
+  const issueBody = core.getInput('issue_body');
+
   const octokit = github.getOctokit(MACHINE_WORKER_TOKEN);
   
-  const issues = await octokit.paginate(
-    "GET /repos/{owner}/{repo}/issues",
-    { owner: "AuctionSoft", repo: "as2-clients" },
-    (response) => response.data.map((issue) => issue.title)
-  );
+  // Get repo content at path
+  const customEnvsData = await octokit.repos.getContent({
+    owner: REPO_OWNER,
+    repo: CLIENTS_REPO,
+    path: CLIENTS_REPO_CUSTOM_ENV_PATH
+  });
 
-  console.log(issues.join(','));
+  /* =============================================
+    Create list of repositories to file issues on
+      -- Get the name of each file
+      -- Trim .env
+      -- Add suffix
+     ============================================== */
+  const customRepos = customEnvsData.data.map((client) => `${client.name.toLowerCase().substr(0, client.name.indexOf('.env'))}-${REPO_KEY_SUFFIX}`);
+
+  // For each repo, file an issue
+  for (const repo of customRepos) {
+    try {
+      await octokit.issues.create({
+        owner: REPO_OWNER,
+        repo: repo,
+        title: issueTitle,
+        body: issueBody,
+      });
+      console.log(`Successfully filed issue on repository "${repo}"`);
+    } catch (e) {
+      console.error(`Error creating issue on repository "${repo}"`);
+    }
+  }
 }
 
 run();
