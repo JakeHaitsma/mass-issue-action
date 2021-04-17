ed11082268ae093dec4b3d8be983b7a52fd91082
Jacob Haitsma <jacobhaitsma@gmail.com>
2021-04-16 20:42:02 -0400

    :package: Pull out more constants, filter out invalid files in repo

--- src/action.js
+++ src/action.js
@@ -6,21 +6,22 @@ async function run() {
   const REPO_OWNER = 'JacobHaitsma';
   const REPO_KEY_SUFFIX = '-auctionsoftware';
   const CLIENTS_REPO = 'test-clients-repo';
-  const CLIENTS_REPO_CUSTOM_ENV_PATH = 'custom_env';
+  const CLIENTS_REPO_ENV_PATH = 'custom_env';
+  const REPO_FILE_EXTENSION = '.env';
 
   // Inputs
   const MACHINE_WORKER_TOKEN = core.getInput('MACHINE_WORKER_TOKEN');
   const issueTitle = core.getInput('issue_title');
   const issueBody = core.getInput('issue_body');
 
-  // Get octokit w/ key
+  // Get octokit w/ token
   const octokit = github.getOctokit(MACHINE_WORKER_TOKEN);
   
   // Get repo content at path
   const customEnvsData = await octokit.repos.getContent({
     owner: REPO_OWNER,
     repo: CLIENTS_REPO,
-    path: CLIENTS_REPO_CUSTOM_ENV_PATH
+    path: CLIENTS_REPO_ENV_PATH
   });
 
   /* =============================================
@@ -29,9 +30,13 @@ async function run() {
       -- Trim .env
       -- Add suffix
      ============================================== */
-  const customRepos = customEnvsData.data.map((client) => `${client.name.toLowerCase().substr(0, client.name.indexOf('.env'))}${REPO_KEY_SUFFIX}`);
+  if (!customEnvs.data) throw new Error(`No data at path "${CLIENT_REPO_ENV_PATH}" on repo "${REPO_OWNER}/${CLIENTS_REPO}"`)
+  const customRepos =
+    customEnvsData.data
+      .filter((client) => client.endsWith(REPO_FILE_EXTENSION))
+      .map((client) => `${client.name.toLowerCase().substr(0, client.name.indexOf(REPO_FILE_EXTENSION))}${REPO_KEY_SUFFIX}`);
 
-  console.log(`💬 Filing issue with title "${issueTitle}" and content "${issueBody}"`)
+  console.log(`💬 Filing issue with title "${issueTitle}" and content "${issueBody}" on ${customRepos.length} repositories...`)
 
   // For each repo, file an issue
   for (const repo of customRepos) {
@@ -40,7 +45,7 @@ async function run() {
         owner: REPO_OWNER,
         repo: repo,
         title: issueTitle,
-        body: issueBody, // supports markdown
+        body: issueBody, // supports markdown, line breaks with <br />
       });
       console.log(`✅ Successfully filed issue on repository "${repo}"`);
     } catch (e) {
